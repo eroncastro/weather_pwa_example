3a297142e15f44197948df6fed23e8f6574e75ab
Eron <eroncastro@gmail.com>
2017-04-04 08:46:14 -0300

    Adds listeners for activate and fetch

--- sw.js
+++ sw.js
@@ -33,3 +33,26 @@ self.addEventListener('install', function(e) {
     })
   );
 });
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  )
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
